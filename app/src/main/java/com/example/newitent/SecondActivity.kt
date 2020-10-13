c66d3dc738486afd39595a89115c42e0ceddd6b0
MozartVieira <mozart.breno@deway.com.br>
2020-10-13 11:11:02 -0300

    fist commit

---
+++ app/src/main/java/com/example/newitent/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.newitent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        tv.setText("Click data : " + intent.getStringExtra("dataClick"))
+    }
+}
